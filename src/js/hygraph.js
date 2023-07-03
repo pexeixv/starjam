335eb990b6da8dc4f8c8a9b8ed507fe6f97fc2b7
pexeixv <gavineditz@gmail.com>
2023-07-03 14:53:14 +0530

    install Hygraph

---
+++ src/js/hygraph.js
@@ -0,0 +1,46 @@
+import { request, gql } from "graphql-request";
+const url = import.meta.env.CONTENT_API;
+
+const queries = {
+  products: gql`
+    query Products {
+      products {
+        title
+        subtitle
+        image {
+          url
+        }
+        slug
+        drumSizes {
+          sizeInInches
+        }
+        drumColours {
+          name
+          colourCode {
+            hex
+          }
+          image {
+            url
+          }
+        }
+        content {
+          html
+        }
+      }
+    }
+  `,
+};
+
+const hygraph = async (query) => {
+  const res = await request(url, queries[query]);
+  const data = await res[query];
+  return data;
+};
+
+export default hygraph;
+
+const json = {
+  drumType: "Rope Weave",
+  drumBody: "Mango Wood Body",
+  image: "https://media.graphassets.com/q3Udffo1RFbx49qRYfdI",
+};
