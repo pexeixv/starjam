2e0a49b5c8f01982300cd830ab7657a023b1182e
pexeixv <gavineditz@gmail.com>
2025-03-25 00:30:51 +0530

    Upgrade to Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://starjam.in",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind(), sitemap()],
 });
